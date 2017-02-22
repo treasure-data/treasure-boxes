b38a3f1a47fc50a317e5201fa608525db90544ab
Keisuke Noda <ksuke.noda@gmail.com>
2017-02-22 18:07:36 +0900

    add td-result-output-redshift

---
+++ td/redshift/queries/sample.sql
@@ -0,0 +1,13 @@
+SELECT 
+  TD_TIME_FORMAT(time,'yyyy-MM-dd HH:mm:ss') as datetime,
+  symbol,
+  open,
+  volume,
+  high,
+  low,
+  close
+FROM 
+  nasdaq
+WHERE
+  TD_TIME_RANGE(time, '2014-01-01','2014-04-01')
+;
