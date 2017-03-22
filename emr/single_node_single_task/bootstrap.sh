a837f66dca8c10f9893a5ca7d730d9256e5850a7
takahashi <torutakahashi.ayashi@gmail.com>
2017-03-22 10:30:33 +0900

    Remove unused comments

--- emr/single_node_single_task/bootstrap.sh
+++ emr/single_node_single_task/bootstrap.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 set -ex
 
-#sudo yum -y install python35 python35-pip python35-virtualenv python35-devel
-#sudo alternatives --set python /usr/bin/python3.5
 pip install pandas pandas-td --user --disable-pip-version-check
