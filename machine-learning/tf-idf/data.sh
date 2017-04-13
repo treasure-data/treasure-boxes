aefc9293a1b9dde5fe21e41cef3f353e9a54a07b
Takuya Kitazawa <k.takuti@gmail.com>
2017-04-13 14:10:03 +0900

    Update data.sh
    * Change table name from `wikipage` -> `docs`
    * Add sample docs file

--- machine-learning/tf-idf/data.sh
+++ machine-learning/tf-idf/data.sh
@@ -5,20 +5,12 @@ if [ -z $(which td) ]; then
   exit 1
 fi
 
-mkdir middle
-cd middle
-
-curl -o tfidf_test.tsv -L https://gist.githubusercontent.com/myui/190b91a3a792ccfceda0/raw/327acd192da4f96da8276dcdff01b19947a4373c/tfidf_test.tsv
-
 td db:create tfidf
-td table:create tfidf wikipage
+td table:create tfidf docs
 td import:auto \
   --format tsv \
   --columns docid,contents \
   --column-types long,string \
   --time-value `date +%s` \
   --delimiter "|" \
-  --auto-create tfidf.wikipage ./tfidf_test.tsv
-
-cd ..
-rm -rf middle
+  --auto-create tfidf.docs ./resources/sample_docs.tsv
