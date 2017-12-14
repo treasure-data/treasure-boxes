392bcef956bc7466e84c2a5c803042e41a3e5aa6
Takuya Kitazawa <k.takuti@gmail.com>
2017-12-14 14:25:43 +0900

    Split train/test samples based on stratified sampling
    The method makes ratio between positive and negative samples even both
    in train and test set.
    
    https://en.wikipedia.org/wiki/Stratified_sampling
    https://gist.github.com/myui/649c08cd2fb951e94eb4fa215eaf9df2

--- machine-learning/sfdc-predictive-analytics/queries/split_experiment_test.sql
+++ machine-learning/sfdc-predictive-analytics/queries/split_experiment_test.sql
@@ -4,5 +4,7 @@ FROM
   samples
 WHERE
   -- only use closed opportunities for evaluation
-  rnd > 0.8 AND is_closed = 1 AND is_primary = 1
+  rank_in_label > (per_label_count * ${train_sample_rate})
+  AND is_closed = 1
+  AND is_primary = 1
 ;
