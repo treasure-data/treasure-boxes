62b93737ce8e3b813764e96e85f5deffc797830e
Toru Takahashi <toru-takahashi@users.noreply.github.com>
2018-03-01 10:52:43 +0900

    Update insert_dedup.sql

--- scenarios/marketo_daily_load/queries/insert_dedup.sql
+++ scenarios/marketo_daily_load/queries/insert_dedup.sql
@@ -3,6 +3,6 @@ SELECT * FROM (
     *,
     row_number() over (partition by ${marketo.dedup_key[i]} ORDER BY time DESC) as rnk
   FROM ${marketo.type[i]}_dedup
-  WHERE TD_TIME_RANGE(time, '${from_date}', null, 'JST')
+  WHERE TD_TIME_RANGE(time, '${marketo.from_date}', null, 'JST')
 ) t1
 WHERE rnk = 1
