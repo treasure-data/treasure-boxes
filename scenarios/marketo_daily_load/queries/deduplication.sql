224db85cd735657d4983d463d63beb3c8e4c5d71
Toru Takahashi <toru-takahashi@users.noreply.github.com>
2018-03-01 10:52:29 +0900

    Update deduplication.sql

--- scenarios/marketo_daily_load/queries/deduplication.sql
+++ scenarios/marketo_daily_load/queries/deduplication.sql
@@ -1,6 +1,6 @@
 DELETE FROM ${marketo.type[i]}_dedup
-WHERE TD_TIME_RANGE(time, '${from_date}', null, 'JST')
+WHERE TD_TIME_RANGE(time, '${marketo.from_date}', null, 'JST')
 AND ${marketo.dedup_key[i]}	in (
   SELECT DISTINCT ${marketo.dedup_key[i]} FROM ${marketo.type[i]}
-  WHERE TD_TIME_RANGE(time, '${from_date}', null, 'JST')
+  WHERE TD_TIME_RANGE(time, '${marketo.from_date}', null, 'JST')
 )
