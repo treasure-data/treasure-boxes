e443bb7aea1a62db82cdc28091b1ad6dea52bb09
Toru Takahashi <torutakahashi.ayashi@gmail.com>
2018-03-08 13:29:29 +0900

    Support dynamic column name generation

---
+++ scenarios/marketo_daily_load/queries/generate_column_names.sql
@@ -0,0 +1,15 @@
+SELECT 
+  array_join(
+    column_name_array,
+    ','
+  ) AS column_names
+FROM (
+    SELECT 
+      array_agg(column_name) AS column_name_array
+    FROM
+      information_schema.columns
+    WHERE
+      table_schema = '${marketo.type[i]}_dedup'
+      AND table_name = '${marketo.type[i]}_dedupbulkload'
+      AND column_name != 'rnk'
+  ) t1
--- scenarios/marketo_daily_load/queries/insert_dedup.sql
+++ scenarios/marketo_daily_load/queries/insert_dedup.sql
@@ -1,4 +1,5 @@
-SELECT * FROM (
+SELECT ${td.last_results.column_names}
+FROM (
   SELECT
     *,
     row_number() over (partition by ${marketo.dedup_key[i]} ORDER BY time DESC) as rnk
