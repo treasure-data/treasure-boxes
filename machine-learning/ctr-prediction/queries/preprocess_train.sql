0a1e6d3a05808d6dca9463b12c0af76ce3b2950c
Takuya Kitazawa <k.takuti@gmail.com>
2018-04-19 15:38:11 +0900

    Remove commented `feature_hashing()`

--- machine-learning/ctr-prediction/queries/preprocess_train.sql
+++ machine-learning/ctr-prediction/queries/preprocess_train.sql
@@ -72,18 +72,16 @@ select
         ${td.last_results.train_max13}
       )
     ),
-    -- feature_hashing(
-      categorical_features(
-        array(
-          "c1", "c2", "c3", "c4", "c5", "c6", "c7", "c8", "c9", "c10",
-          "c11", "c12", "c13", "c14", "c15", "c16", "c17", "c18", "c19", "c20",
-          "c21", "c22", "c23", "c24", "c25", "c26"
-        ),
-        c1, c2, c3, c4, c5, c6, c7, c8, c9, c10,
-        c11, c12, c13, c14, c15, c16, c17, c18, c19, c20,
-        c21, c22, c23, c24, c25, c26
-      )
-    -- )
+    categorical_features(
+      array(
+        "c1", "c2", "c3", "c4", "c5", "c6", "c7", "c8", "c9", "c10",
+        "c11", "c12", "c13", "c14", "c15", "c16", "c17", "c18", "c19", "c20",
+        "c21", "c22", "c23", "c24", "c25", "c26"
+      ),
+      c1, c2, c3, c4, c5, c6, c7, c8, c9, c10,
+      c11, c12, c13, c14, c15, c16, c17, c18, c19, c20,
+      c21, c22, c23, c24, c25, c26
+    )
   ) as features,
   label
 from
