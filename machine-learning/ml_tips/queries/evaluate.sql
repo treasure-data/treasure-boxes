c7279e5f6d32bcbc1d45009abc138528fa8be975
Aki Ariga <ariga@treasure-data.com>
2019-03-06 14:44:53 +0900

    implement oversampling

--- machine-learning/ml_tips/queries/evaluate.sql
+++ machine-learning/ml_tips/queries/evaluate.sql
@@ -1,15 +1,15 @@
 select
   auc(${predicted_column}, survived) as auc
   , logloss(${predicted_column}, survived) as logloss
+from
+  (
+    select
+      p.${predicted_column}, t.survived
     from
-      (
-  select
-    p.${predicted_column}, t.survived
-  from
-    ${predicted_table} p
-  join
-    ${actual} t on (p.rowid = t.rowid)
-  order by
-    probability desc
-) t2
+      ${predicted_table} p
+    join
+      ${actual} t on (p.rowid = t.rowid)
+    order by
+      probability desc
+  ) t2
 ;
--- machine-learning/ml_tips/queries/train_classifier.sql
+++ machine-learning/ml_tips/queries/train_classifier.sql
@@ -1,8 +1,32 @@
-select
-  train_classifier(
+with amplified as (
+  select
+    amplify(${oversample_n_times}, features, survived) as (features, survived)
+  from
+    ${source}
+),
+train_oversampled as (
+  select
     features
     , survived
-  ) as (feature, weight)
+  from
+    amplified
+  CLUSTER BY rand(43)
+),
+model_oversampled as (
+  select
+    train_classifier(
+      features
+      , survived
+    ) as (feature, weight)
+  from
+    train_oversampled
+)
+-- DIGDAG_INSERT_LINE
+select
+  feature
+  , avg(weight) as weight
 from
-  ${source}
+  model_oversampled
+group by
+  feature
 ;
