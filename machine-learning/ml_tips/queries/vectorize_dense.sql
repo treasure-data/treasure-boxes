997d035374c1fd36fbe7fd8ee8eea50ec2dea38d
Aki Ariga <ariga@treasure-data.com>
2019-04-02 15:20:51 -0700

    doc: add note for random forest workaround

--- machine-learning/ml_tips/queries/vectorize_dense.sql
+++ machine-learning/ml_tips/queries/vectorize_dense.sql
@@ -1,7 +1,11 @@
 -- client: molehill/0.0.1
 select
   rowid
-  , array(age, fare, mhash(embarked, ${feature_cardinality}), mhash(sex, ${feature_cardinality}), mhash(pclass, ${feature_cardinality})) as features
+  , array(
+    age, fare
+    -- `feature_cardinality` is a workaround to avoid this issue: https://issues.apache.org/jira/projects/HIVEMALL/issues/HIVEMALL-243
+    , mhash(embarked, ${feature_cardinality}), mhash(sex, ${feature_cardinality}), mhash(pclass, ${feature_cardinality})
+  ) as features
   , survived
 from
   ${source}
