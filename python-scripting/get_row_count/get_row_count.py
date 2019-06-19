20c7ed0bea77acdaff6c82a7e3eed62665d0b004
ryutaroy <ryuyada@gmail.com>
2019-06-19 16:28:04 +0900

    Modified get_row_count, add READEME.md

--- python-scripting/get_row_count/get_row_count.py
+++ python-scripting/get_row_count/get_row_count.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# This script get row count numbers from all tables of all databases,
-# and import the result to specified db/table.
-
 import sys
 import os
 import os.path
@@ -13,17 +10,15 @@ import pytd
 import pandas as pd
 
 TD_APIKEY=os.environ.get('td_apikey')
-DEST_DB='DB_NAME'
-DEST_TABLE='TABLE_NAME'
+TD_ENDPOINT=os.environ.get('td_endpoint')
 
-#import pdb;pdb.set_trace()
-def get_row_count():
+def get_row_count(dest_db: str, dest_table: str):
   df = pd.DataFrame( columns=['db_name','table_name','row_count'] )
-  client = pytd.Client(apikey=TD_APIKEY, endpoint='https://api.treasuredata.com/', database=DEST_DB, engine='presto')
+  client = pytd.Client(apikey=TD_APIKEY, endpoint=TD_ENDPOINT, database=dest_db, engine='presto')
   for db in client.list_databases():
     for table in client.list_tables(db.name):
       tmp_se = pd.Series( [ db.name, table.name, table.count ], index=df.columns )
       df = df.append( tmp_se, ignore_index=True )
       #print(db.name + ',' + table.name + ',' + str(table.count))
   #print(df)
-  client.load_table_from_dataframe(df, DEST_TABLE, if_exists='append')
+  client.load_table_from_dataframe(df, dest_table, if_exists='append')
