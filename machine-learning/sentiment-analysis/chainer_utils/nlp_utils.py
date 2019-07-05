613fc007c14f9433270a8e988c1337a9608512f7
Aki Ariga <ariga@treasure-data.com>
2019-07-05 19:20:44 +0900

    feat: add a chainer example of sentiment analysis

---
+++ machine-learning/sentiment-analysis/chainer_utils/nlp_utils.py
@@ -0,0 +1,84 @@
+# Original code is under https://github.com/chainer/chainer/blob/v6.1.0/examples/text_classification/
+import collections
+import io
+
+import chainer
+import numpy
+
+
+def split_text(text, char_based=False):
+    if char_based:
+        return list(text)
+    else:
+        return text.split()
+
+
+def normalize_text(text):
+    return text.strip().lower()
+
+
+def make_vocab(dataset, max_vocab_size=20000, min_freq=2):
+    counts = collections.defaultdict(int)
+    for tokens, _ in dataset:
+        for token in tokens:
+            counts[token] += 1
+
+    vocab = {"<eos>": 0, "<unk>": 1}
+    for w, c in sorted(counts.items(), key=lambda x: (-x[1], x[0])):
+        if len(vocab) >= max_vocab_size or c < min_freq:
+            break
+        vocab[w] = len(vocab)
+    return vocab
+
+
+def read_vocab_list(path, max_vocab_size=20000):
+    vocab = {"<eos>": 0, "<unk>": 1}
+    with io.open(path, encoding="utf-8", errors="ignore") as f:
+        for l in f:
+            w = l.strip()
+            if w not in vocab and w:
+                vocab[w] = len(vocab)
+            if len(vocab) >= max_vocab_size:
+                break
+    return vocab
+
+
+def make_array(tokens, vocab, add_eos=True):
+    unk_id = vocab["<unk>"]
+    eos_id = vocab["<eos>"]
+    ids = [vocab.get(token, unk_id) for token in tokens]
+    if add_eos:
+        ids.append(eos_id)
+    return numpy.array(ids, numpy.int32)
+
+
+def transform_to_array(dataset, vocab, with_label=True):
+    if with_label:
+        return [
+            (make_array(tokens, vocab), numpy.array([cls], numpy.int32))
+            for tokens, cls in dataset
+        ]
+    else:
+        return [make_array(tokens, vocab) for tokens in dataset]
+
+
+@chainer.dataset.converter()
+def convert_seq(batch, device=None, with_label=True):
+    def to_device_batch(batch):
+        if device is None:
+            return batch
+        src_xp = chainer.backend.get_array_module(*batch)
+        xp = device.xp
+        concat = src_xp.concatenate(batch, axis=0)
+        sections = numpy.cumsum([len(x) for x in batch[:-1]], dtype=numpy.int32)
+        concat_dev = chainer.dataset.to_device(device, concat)
+        batch_dev = xp.split(concat_dev, sections)
+        return batch_dev
+
+    if with_label:
+        return {
+            "xs": to_device_batch([x for x, _ in batch]),
+            "ys": to_device_batch([y for _, y in batch]),
+        }
+    else:
+        return to_device_batch([x for x in batch])
---
+++ machine-learning/sentiment-analysis/predict_chainer.py
@@ -0,0 +1,153 @@
+import json
+import os
+import tarfile
+from logging import DEBUG, StreamHandler, getLogger
+
+import chainer
+import numpy
+import pytd.pandas_td as td
+from pytd.writer import SparkWriter
+from td_pyspark import TDSparkContextBuilder
+
+from chainer_utils import nets, nlp_utils
+
+MODEL_URL = "https://workflow-example-public.s3.amazonaws.com/imdb_model.tar.gz"
+
+logger = getLogger(__name__)
+handler = StreamHandler()
+handler.setLevel(DEBUG)
+logger.setLevel(DEBUG)
+logger.addHandler(handler)
+logger.propagate = False
+
+
+def setup_model(device, model_setup):
+    setup = json.load(open(model_setup))
+    logger.info(json.dumps(setup, indent=2) + "\n")
+
+    vocab = json.load(open(setup["vocab_path"]))
+    n_class = setup["n_class"]
+
+    # Setup a model
+    if setup["model"] == "rnn":
+        Encoder = nets.RNNEncoder
+    elif setup["model"] == "cnn":
+        Encoder = nets.CNNEncoder
+    elif setup["model"] == "bow":
+        Encoder = nets.BOWMLPEncoder
+    encoder = Encoder(
+        n_layers=setup["layer"],
+        n_vocab=len(vocab),
+        n_units=setup["unit"],
+        dropout=setup["dropout"],
+    )
+    model = nets.TextClassifier(encoder, n_class)
+    chainer.serializers.load_npz(setup["model_path"], model)
+    model.to_device(device)  # Copy the model to the device
+
+    return model, vocab, setup
+
+
+def run_batch(
+    database, input_table, output_table, device, model, vocab, setup, batchsize=64
+):
+    def predict_batch(words_batch):
+        xs = nlp_utils.transform_to_array(words_batch, vocab, with_label=False)
+        xs = nlp_utils.convert_seq(xs, device=device, with_label=False)
+        with chainer.using_config("train", False), chainer.no_backprop_mode():
+            probs = model.predict(xs, softmax=True)
+
+        # Note: Prediction labels are different from original Chainer example
+        #       positive: 1, negative: 0
+        answers = model.xp.argmax(probs, axis=1)
+        scores = probs[model.xp.arange(answers.size), answers].tolist()
+
+        return answers, scores
+
+    td_api_key = os.environ["TD_API_KEY"]
+    endpoint = os.environ["TD_API_SERVER"]
+    jar_path = TDSparkContextBuilder.default_jar_path()
+
+    logger.info("Connect to Treasure Data")
+
+    writer = SparkWriter(apikey=td_api_key, endpoint=endpoint, td_spark_path=jar_path)
+    con = td.connect(apikey=td_api_key, endpoint=endpoint, writer=writer)
+    presto = td.create_engine(f"presto:{database}", con=con)
+
+    logger.info("Fetch data from Treasure Data")
+    test_df = td.read_td(
+        f"""
+        select
+            rowid, sentence, sentiment, polarity
+        from
+            {input_table}
+    """,
+        presto,
+    )
+
+    sentences = test_df["sentence"].tolist()
+
+    logger.info("Start prediction")
+    batch = []
+    predicted = []
+    for sentence in sentences:
+        text = nlp_utils.normalize_text(sentence)
+        words = nlp_utils.split_text(text, char_based=setup["char_based"])
+        batch.append(words)
+        if len(batch) >= batchsize:
+            _predicted, _ = predict_batch(batch)
+            predicted.append(_predicted)
+            batch = []
+
+    if batch:
+        _predicted, _ = predict_batch(batch)
+        predicted.append(_predicted)
+
+    logger.info("Finish prediction")
+
+    test_df["predicted_polarity"] = numpy.concatenate(predicted, axis=None)
+
+    # Note: Train test split strategy is different from pre trained model and
+    #       these tables so that the model includes test data since the model
+    #       is trained by Chainer official example.
+    #       This accuracy is just for a demo.
+    #
+    # accuracy = (test_df.polarity == test_df.predicted_polarity).value_counts()[
+    #     1
+    # ] / len(test_df)
+    # print(f"Test set accuracy: {accuracy}")
+
+    td.to_td(
+        test_df[["rowid", "predicted_polarity"]],
+        f"{database}.{output_table}",
+        con=con,
+        if_exists="replace",
+        index=False,
+    )
+
+    logger.info("Upload completed")
+
+
+def download_model():
+    path = chainer.dataset.cached_download(MODEL_URL)
+    tf = tarfile.open(path, "r")
+    tf.extractall()
+    return os.path.join("result", "args.json")
+
+
+def predict_chainer(database, input_table, output_table, device_num=-1):
+    device = chainer.get_device(device_num)
+    device.use()
+
+    model_setup = download_model()
+    logger.info(f"model setup path: {model_setup}")
+    model, vocab, setup = setup_model(device, model_setup)
+    run_batch(
+        database, input_table, output_table, device, model, vocab, setup, batchsize=128
+    )
+
+
+if __name__ == "__main__":
+    predict_chainer(
+        "sentiment", "movie_review_test_shuffled", "test_predicted_polarities_chainer"
+    )
