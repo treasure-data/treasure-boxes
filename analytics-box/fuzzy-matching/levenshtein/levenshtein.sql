2ccbd5c228303c80289ea7e8e30a8146c703ecc0
PrachiChavan14 <53314563+PrachiChavan14@users.noreply.github.com>
2019-08-01 10:05:10 -0700

    Update levenshtein.sql

--- analytics-box/fuzzy-matching/levenshtein/levenshtein.sql
+++ analytics-box/fuzzy-matching/levenshtein/levenshtein.sql
@@ -2,7 +2,7 @@
 
 with temp as (
 select last_first_name,master_key,group_key,((length(last_first_name) + length(master_key)) - LEVENSHTEIN(last_first_name,master_key))/(length(last_first_name) + length(master_key))*100 as levenshtein_ratio , seq_nbr
-from fuzzy_data
+from ${td.table}
 )
 
 insert OVERWRITE table temp_table select * from temp
