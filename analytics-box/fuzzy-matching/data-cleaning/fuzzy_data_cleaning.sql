13a7da8b6781a17f4bd9c755e85f5c99dff809ec
PrachiChavan14 <53314563+PrachiChavan14@users.noreply.github.com>
2019-08-01 10:03:55 -0700

    Update fuzzy_data_cleaning.sql

--- analytics-box/fuzzy-matching/data-cleaning/fuzzy_data_cleaning.sql
+++ analytics-box/fuzzy-matching/data-cleaning/fuzzy_data_cleaning.sql
@@ -1,5 +1,5 @@
 -- Data Cleaning
 
-insert OVERWRITE table temp_table select LOWER(REGEXP_REPLACE(last_first_name,"[^a-zA-Z]","")) as last_first_name,
-RANK() OVER (ORDER BY last_first_name asc) as seq_nbr
-from fuzzy_data
+insert OVERWRITE table temp_table select LOWER(REGEXP_REPLACE(${td.column},"[^a-zA-Z]","")) as last_first_name,
+RANK() OVER (ORDER BY ${td.column} asc) as seq_nbr
+from ${td.table}
