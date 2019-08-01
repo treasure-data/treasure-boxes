2e69692b992e760e663e2cdaa658152e5871005e
PrachiChavan14 <53314563+PrachiChavan14@users.noreply.github.com>
2019-08-01 10:05:46 -0700

    Update master_key.sql

--- analytics-box/fuzzy-matching/master-key/master_key.sql
+++ analytics-box/fuzzy-matching/master-key/master_key.sql
@@ -4,10 +4,10 @@ with  temp as(
 select last_first_name , group_key from (
     select *, 
     ROW_NUMBER() over (partition by group_key order by group_key desc) rn
-    from fuzzy_data
+    from ${td.table}
 ) t1 where rn = 1
 )
 
 insert OVERWRITE table temp_table select fuzzy_data.last_first_name,fuzzy_data.group_key,temp.last_first_name as master_key , fuzzy_data.seq_nbr as seq_nbr
-from temp , fuzzy_data 
+from temp , ${td.table} fuzzy_data 
 where temp.group_key = fuzzy_data.group_key
