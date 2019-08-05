7ffcac0f4c78abf35ba5f99c0b533a645dfed521
Scott Mitchell <scottmitchell@TD-0385.local>
2019-08-05 15:38:21 -0700

    Add email validation query and workflow

---
+++ analytics-box/validate_email/validate_email.sql
@@ -0,0 +1,31 @@
+SELECT
+${td.column},
+
+-- makes sure there is 1 and only 1 @ symbol .
+CASE WHEN CARDINALITY(SPLIT(${td.column}, '@')) != 2
+
+-- email is not null
+OR ${td.column} IS NULL
+
+-- only have ASCII characters in in any portion of the email address
+OR REGEXP_LIKE(${td.column}, '^[\x21-\x7E]+$') = false
+
+-- the @ symbol is not the first character in the email address or the last
+OR SUBSTR(${td.column}, 1, 1) = '@'
+OR SUBSTR(${td.column}, LENGTH(${td.column}), 1) = '@'
+
+-- a period is not the first or last character in the email
+OR SUBSTR(${td.column}, 1, 1) = '.'
+OR SUBSTR(${td.column}, LENGTH(${td.column}), 1) = '.'
+
+-- email address is > 6 characters
+OR LENGTH(${td.column}) <= 6
+
+-- makes sure there are no ( , \  ; * # ") in the email address
+OR LENGTH(REGEXP_REPLACE(${td.column}, ',|;|#|\*|\\|\"| ')) != LENGTH(${td.column})
+
+-- the email should be formatted as x@x.xx 
+OR REGEXP_LIKE(${td.column}, '..*@..*\...*') = false
+THEN 0
+ELSE 1 END AS valid_email
+FROM ${td.database}.${td.table}
