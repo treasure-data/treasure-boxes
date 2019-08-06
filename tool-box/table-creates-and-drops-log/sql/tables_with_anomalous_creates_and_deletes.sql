76c1ab24d83f2717fdbbc63acd1376800790d759
Max King <max.king@treasure-data.com>
2019-08-06 11:57:51 -0500

    deleting all updates to attempt to bring fork up to date

--- tool-box/table-creates-and-drops-log/sql/tables_with_anomalous_creates_and_deletes.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-  a.event_name, 
-  a.resource_name, 
-  count(1) AS change_count
-FROM td_audit_log.access AS a
-JOIN ${td.database}.counts_of_table_drops_and_creates_last_${anomaly_range_days}_days AS b              
-ON a.resource_name = b.resource_name AND a.event_name = b.event_name
-WHERE a.time > TO_UNIXTIME(date_trunc('day',now()))-(86400*${lookback_range_days})
-GROUP BY a.event_name, a.resource_name
