127ba88a0421ffead8e913b3e93e7dde7c7038dd
Aki Ariga <ariga@treasure-data.com>
2019-12-04 18:59:16 +0900

    feat: migrate to TensorFlow v2

--- machine-learning-box/sentiment-analysis/py_scripts/data.py
+++ machine-learning-box/sentiment-analysis/py_scripts/data.py
@@ -6,6 +6,7 @@ from urllib.request import urlopen
 
 os.system(f"{sys.executable} -m pip install -U pytd==0.8.0 td-client")
 
+
 def convert_directory_to_csv(directory, polarity, out_file_path):
     with open(out_file_path, "a") as csvfile:
         writer = csv.writer(csvfile)
