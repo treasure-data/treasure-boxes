9d320656c88cd78593bc0a1340a8700759cc9589
Makoto Yui <myui@apache.org>
2019-12-26 23:56:32 +0900

    Changed age range

--- machine-learning-box/gender_age_prediction/queries/blogposts/gender_age.sql
+++ machine-learning-box/gender_age_prediction/queries/blogposts/gender_age.sql
@@ -1,7 +1,12 @@
 select
   userid,
-  (if(gender='male','M','F') || cast(cast(round(age / 5) as int) * 5 as varchar)
-  ) as gender_age
+  (if(gender='male','M','F') ||
+        CASE
+           WHEN age >= 35 THEN '35'
+           WHEN age >= 25 THEN '25'
+           ELSE cast(cast(round(age / 5) as int) * 5 as varchar)
+        END
+  ) as gender_age -- 35~, 25~, 20~, 15~, 10~
 from
   blogposts
 ;
