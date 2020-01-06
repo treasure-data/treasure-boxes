b8e136470954ffb832184355af4ba37cb66c8590
Makoto Yui <myui@apache.org>
2020-01-06 15:49:33 +0900

    Reverted changes

--- machine-learning-box/gender_age_prediction/queries/rf/confusion_matrix_pivot.sql
+++ machine-learning-box/gender_age_prediction/queries/rf/confusion_matrix_pivot.sql
@@ -2,19 +2,23 @@
 WITH tmp as (
   SELECT
     actual,
-	max(CASE WHEN predicted = 'F10x' THEN cnt ELSE 0 END) AS F10x,
-	max(CASE WHEN predicted = 'F20x' THEN cnt ELSE 0 END) AS F20x,
-	max(CASE WHEN predicted = 'F30x' THEN cnt ELSE 0 END) AS F30x,
-	max(CASE WHEN predicted = 'M10x' THEN cnt ELSE 0 END) AS M10x,
-	max(CASE WHEN predicted = 'M20x' THEN cnt ELSE 0 END) AS M20x,
-	max(CASE WHEN predicted = 'M30x' THEN cnt ELSE 0 END) AS M30x
+	max(CASE WHEN predicted = 'F15' THEN cnt ELSE 0 END) AS F15,
+	max(CASE WHEN predicted = 'F20' THEN cnt ELSE 0 END) AS F20,
+	max(CASE WHEN predicted = 'F25' THEN cnt ELSE 0 END) AS F25,
+	max(CASE WHEN predicted = 'F35' THEN cnt ELSE 0 END) AS F35,
+	max(CASE WHEN predicted = 'F50' THEN cnt ELSE 0 END) AS F50,
+	max(CASE WHEN predicted = 'M15' THEN cnt ELSE 0 END) AS M15,
+	max(CASE WHEN predicted = 'M20' THEN cnt ELSE 0 END) AS M20,
+	max(CASE WHEN predicted = 'M25' THEN cnt ELSE 0 END) AS M25,
+	max(CASE WHEN predicted = 'M35' THEN cnt ELSE 0 END) AS M35,
+	max(CASE WHEN predicted = 'M50' THEN cnt ELSE 0 END) AS M50
   FROM rf_confusion_matrix
   GROUP BY actual
 )
 select 
   actual,
-  F10x,F20x,F30x,
-  M10x,M20x,M30x
+  F15,F20,F25,F35,F50,
+  M15,M20,M25,M35,M50
 from
   tmp
 order by
