91255ec41cbafeb7dae848aac056bf42defe24d3
Aki Ariga <ariga@treasure-data.com>
2020-02-18 17:17:33 +0900

    refactor: stop appending dataframe

--- integration-box/rss/py_scripts/tasks.py
+++ integration-box/rss/py_scripts/tasks.py
@@ -9,29 +9,21 @@ import pandas as pd
 os.system(f"{sys.executable} -m pip install feedparser")
 os.system(f"{sys.executable} -m pip install -U pytd==1.0.0")
 
-import pytd
-import feedparser
-
-
 TD_APIKEY = os.environ.get("td_apikey")
 TD_ENDPOINT = os.environ.get("td_endpoint")
 
 
 def rss_import(dest_db: str, dest_table: str, rss_url_list):
-    df = pd.DataFrame(columns=["title", "description", "link"])
+    import pytd
+    import feedparser
+
+    posts = []
     for rss_url in rss_url_list:
         d = feedparser.parse(rss_url)
         for entry in d.entries:
-            tmp_se = pd.Series(
-                [entry.title, entry.description, entry.link], index=df.columns
-            )
-            df = df.append(tmp_se, ignore_index=True)
+            posts.append((entry.title, entry.description, entry.link))
 
-    client = pytd.Client(
-        apikey=TD_APIKEY,
-        endpoint=TD_ENDPOINT,
-        database=dest_db,
-        default_engine="presto",
-    )
+    df = pd.DataFrame(posts, columns=["title", "description", "link"])
+    client = pytd.Client(apikey=TD_APIKEY, endpoint=TD_ENDPOINT, database=dest_db)
     client.create_database_if_not_exists(dest_db)
     client.load_table_from_dataframe(df, dest_table, if_exists="append")
