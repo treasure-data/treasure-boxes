a0a04e9980ee1c9de2c1cadc3da20a739caee19f
waysaku <waysaku@gmail.com>
2020-04-10 18:43:06 +0900

    added new yahoo-dmp integration workflow

---
+++ integration-box/yahoo-dmp/queries/userlist.sql
@@ -0,0 +1,6 @@
+SELECT
+  td_client_id
+FROM
+  demo_users
+LIMIT 1000
+
---
+++ integration-box/yahoo-dmp/scripts/put_userlist.py
@@ -0,0 +1,21 @@
+import os
+import sys
+import requests
+
+os.system(f"{sys.executable} -m pip install -U pytd==1.0.0 td-client")
+import pytd
+import pandas as pd
+
+def upload(sqlfile, database, presigned_url):
+  with open(sqlfile) as f:
+    querytxt = f.read()
+
+  client = pytd.Client(apikey=os.getenv('td_apikey'), database=database)
+  res = client.query(querytxt)
+  df = pd.DataFrame(**res)
+  csv = df.to_csv(index=False)
+
+  requests.put(presigned_url, data=csv)
+
+
+
