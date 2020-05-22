f0f72fa92995ced42328143b9cb2659c31a20b67
waysaku <waysaku@gmail.com>
2020-05-22 13:17:22 +0900

    added logger

--- integration-box/yahoo-dmp/scripts/put_userlist.py
+++ integration-box/yahoo-dmp/scripts/put_userlist.py
@@ -1,10 +1,19 @@
 import os
 import sys
 import requests
+from logging import DEBUG, INFO, StreamHandler, getLogger
 
 os.system(f"{sys.executable} -m pip install -U pytd==1.3.0 td-client")
 import pytd
 import pandas as pd
+
+logger = getLogger(__name__)
+handler = StreamHandler()
+handler.setLevel(INFO)
+logger.setLevel(INFO)
+logger.addHandler(handler)
+logger.propagate = False
+
 
 def upload(sqlfile, database, presigned_url):
   with open(sqlfile) as f:
@@ -15,4 +24,11 @@ def upload(sqlfile, database, presigned_url):
   df = pd.DataFrame(**res)
   csv = df.to_csv(index=False)
 
-  requests.put(presigned_url, data=csv)
+  res = requests.put(presigned_url, data=csv)
+
+  if res.status_code != 200:
+    logger.error(f"Failed to call Yahoo API with http status code {res.status_code}")
+    logger.error(res.text)
+    sys.exit(os.EX_DATAERR)
+  else:
+    print(f"Succeeded calling Yahoo API with http status code {res.status_code}")
