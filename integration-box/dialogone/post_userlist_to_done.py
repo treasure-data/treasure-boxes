be6db1a2f80f66d807bedee9c2938f84486787c7
takuya-obata <88311084+takuya-obata@users.noreply.github.com>
2022-09-27 12:32:58 +0900

    Revise for New API Version

--- integration-box/dialogone/post_userlist_to_done.py
+++ integration-box/dialogone/post_userlist_to_done.py
@@ -1,17 +1,13 @@
 import os
 import sys
 os.system(f"{sys.executable} -m pip install -U pytd==1.4.0 td-client")
-os.system(f"{sys.executable} -m pip install -U google-auth==1.34.0")
 
-from google.auth import jwt
-from google.auth import crypt
 import pandas as pd
 import pytd
 import requests
 from logging import INFO, StreamHandler, getLogger
 import gzip
-import time
-
+import hashlib
 
 logger = getLogger(__name__)
 handler = StreamHandler()
@@ -20,73 +16,50 @@ logger.setLevel(INFO)
 logger.addHandler(handler)
 logger.propagate = False
 
-expiry_length = 3600
-interval_length = 1
 
-def upload_user_list(database, table, user_id_column, filename, sa_email, acid):
-    """Upload a user list for creating Done segments."""
+def upload(**kwargs):
+    acid = kwargs.get('acid') 
+    api_key = kwargs.get('api_key')
+    service_id = kwargs.get('service_id')
 
-    csvs = []
-    row_len_limit = 50000000 # max data length per a API call
-    file_name_limit = 255
     endpoint = 'https://line-api.dialogone.jp/v1/line/external-segments/file'
+    url = f"{endpoint}/{service_id}/{acid}"
 
-    apikey = os.getenv("TD_API_KEY")
-    td_endpoint = os.getenv("TD_ENDPOINT")
-    if td_endpoint is None:
-        td_endpoint = 'api.treasuredata.com'
+    database = kwargs.get('database')
+    table = kwargs.get('table')
+    column = kwargs.get('user_id_column')
+    filename = kwargs.get('filename')
 
-    sql = retrive_user_id_sql(table, user_id_column)
-    logger.info("---- sql ----")
-    logger.info(f"\n{sql}\n")
-    logger.info("-------------")    
-       
-    client = pytd.Client(apikey=apikey, endpoint=td_endpoint, database=database)
-    res = client.query(sql)   
-    df = pd.DataFrame(**res)  
+    if (len(filename) > 255):
+        logger.error(
+          f"Filename must be less than 255 characters."
+        )
+        sys.exit(os.EX_DATAERR)
+
+    # get user list from Treasure Data in CSV format 
+    csvs = retrive_user_id_csv(database, table, column)
     
-    while (len(df) > row_len_limit):
-        d = df[:row_len_limit]
-        r = df[row_len_limit:]
-        csv = d.to_csv(header=False, index=False)
-        csvs.append(csv)
-        df = r
-
-    if (len(df) > 0):
-        csv = df.to_csv(header=False, index=False)
-        csvs.append(csv)
-
     for i, csv in enumerate(csvs):
-
-        logger.info("---- user list as first 10 lines----")
-        logger.info("\n".join(csv.splitlines()[:10]))
-        logger.info("---- Total number of IDs = " + str(len(csv.splitlines())) + "----")
-
         idx = i + 1
         file_name = filename
         if (len(csvs) > 1):
-            file_name = "{}_{}".format(filename, idx)
+            file_name = f"{filename}_{idx}"
+        file_name = f"{file_name}.csv" 
 
-        if (len(file_name) > file_name_limit):
-            logger.error(
-                f"Filename must be less than 255 characters."
-            )
-            sys.exit(os.EX_DATAERR)            
-
-        # generate a signed JWT
-        token = generate_jwt(sa_email)
-
-        # post request to Done API
-        files = {
-            'acid': (None, acid),
-            'file': (f"{file_name}.csv", csv, 'text/csv')
+        # post request to Done API     
+        token = hashlib.sha256(f"{api_key}{file_name}".encode("utf-8")).hexdigest()
+        headers = {
+            "X-DialogOne-Access-Token": token
         }
-        headers = {"Authorization": "Bearer {}".format(token.decode())}
 
+        files = {
+            'file': (file_name, csv, "text/csv")
+        }
+        
         res = requests.post(
-            endpoint,
-            files=files,
+            url,
             headers=headers,
+            files=files 
         )
 
         if res.status_code != 200:
@@ -101,37 +74,42 @@ def upload_user_list(database, table, user_id_column, filename, sa_email, acid):
             )
 
         if csvs[idx:idx + 1]:
-            logger.debug('*** Waiting till next call ***')
-            time.sleep(interval_length)
+            logger.debug('*** Waiting 60 seconds till next call ***')
+            time.sleep(60)
 
-def retrive_user_id_sql(table, column):
-    return f"""
-    SELECT
-      {column}
-    FROM 
-      {table}
-    WHERE 
-      {column} is not null
-      AND REGEXP_LIKE({column}, 'U[0-9a-f]{{{32}}}')
-    """
+def retrive_user_id_csv(database, table, column):
+    csvs = []
+    row_len_limit = 50000000 # max data length per a API call
 
-def generate_jwt(sa_email):
-    """Generates a signed JSON Web Token using a Google API Service Account."""
+    client = pytd.Client(apikey=os.getenv("TD_API_KEY"), database=database)
+    query = f"""
+        SELECT
+            {column}
+        FROM 
+            {table}
+        WHERE 
+            {column} is not null
+            AND REGEXP_LIKE({column}, 'U[0-9a-f]{{{32}}}')
+        """
 
-    now = int(time.time())
+    res = client.query(query)
+    df = pd.DataFrame(**res)  
 
-    # build payload
-    payload = {
-        'iat': now,
-        "exp": now + expiry_length,
-        'iss': sa_email,
-        'aud':  sa_email,
-        'sub': sa_email,
-        'email': sa_email
-    }
+    while (len(df) > row_len_limit):
+        d = df[:row_len_limit]
+        r = df[row_len_limit:]
+        csv = d.to_csv(header=False, index=False)
+        
+        logger.info("---- user list as first 10 lines----")
+        logger.info("\n".join(csv.splitlines()[:10]))
+        logger.info("---- Total number of IDs = " +
+                str(len(csv.splitlines())) + "----")
 
-    # sign with private key
-    signer = crypt.RSASigner.from_string(os.getenv("PRIVATE_KEY").replace('\\n', '\n'), os.getenv("PRIVATE_KEY_ID"))
-    token = jwt.encode(signer, payload)
+        csvs.append(csv)
+        df = r
 
-    return token
+    if (len(df) > 0):
+        csv = df.to_csv(header=False, index=False)
+        csvs.append(csv)
+
+    return csvs
