8a0011a72f6c7a67fa5f00c25ce32bab1c40a30d
o-mura <yuu.ohmura@treasure-data.com>
2023-02-14 17:28:05 +0900

    add monitoring workflow (session/attempt/workflow/project...)

---
+++ scenarios/monitoring/workflow_monitoring/scripts/ingest_session.py
@@ -0,0 +1,46 @@
+# https://github.com/treasure-data/digdag/blob/master/digdag-server/src/main/java/io/digdag/server/rs/SessionResource.java#L78
+# 100件/1ページがmaxなので毎回洗い替えというような実装は無理
+
+import requests
+import pandas as pd
+import pytd
+import os
+from datetime import datetime as dt
+
+def convert(session_time):
+    return int(dt.strptime(session_time, '%Y-%m-%dT%H:%M:%S%z').timestamp())
+
+def get_sessions1(url, headers):
+    print(url)
+    res = requests.get(url=url, headers= headers)
+    if res.status_code != requests.codes.ok:
+        res.raise_for_status()
+    
+    return res.json()['sessions']
+
+def get_all_session(base_url, headers, count, lower_limit_session_id):
+    ses_list = list()
+
+    url = base_url % (count)
+    ses = get_sessions1(url, headers)
+    while len(ses) == count and (int(ses[-1]['id']) > int(lower_limit_session_id)):
+        ses_list.extend(ses)
+        url = (base_url + '&last_id=%s') % (count, ses[-1]['id'])
+        ses = get_sessions1(url, headers)
+    
+    ses_list.extend(ses)
+    return ses_list
+    
+def run(session_unixtime, dest_db, dest_table, api_endpoint='api.treasuredata.com', workflow_endpoint='api-workflow.treasuredata.com', count=100, lower_limit_session_id='9999999999', if_exists='append'):
+    workflow_url = 'https://%s/api/sessions' % workflow_endpoint + '?page_size=%d'
+    headers = {'Authorization': 'TD1 %s' % os.environ['TD_API_KEY']}
+    ses_list = get_all_session(workflow_url, headers, count, lower_limit_session_id)
+    if len(ses_list) == 0:
+      print('no import record')
+      return
+    df = pd.DataFrame(ses_list)
+    df['time'] = df['sessionTime'].apply(convert)
+    df['id'] = df['id'].astype('int')
+    df = df[df['id'] > int(lower_limit_session_id)]
+    client = pytd.Client(apikey=os.environ['TD_API_KEY'], endpoint='https://%s' % api_endpoint, database=dest_db)
+    client.load_table_from_dataframe(df, dest_table, if_exists=if_exists, fmt='msgpack')
---
+++ scenarios/monitoring/workflow_monitoring/scripts/ingest_workflow.py
@@ -0,0 +1,37 @@
+# https://github.com/treasure-data/digdag/blob/master/digdag-server/src/main/java/io/digdag/server/rs/WorkflowResource.java#L109
+
+import requests
+import pandas as pd
+import pytd
+import os
+
+def get_workflows1(url, headers):
+    print(url)
+    res = requests.get(url=url, headers= headers)
+    if res.status_code != requests.codes.ok:
+        res.raise_for_status()
+    
+    return res.json()['workflows']
+
+def get_all_workflow(base_url, headers, count):
+    wfs_list = list()
+
+    url = base_url % (count, '0')
+    wfs = get_workflows1(url, headers)
+
+    while len(wfs) == count:
+        wfs_list.extend(wfs)
+        url = base_url % (count, wfs[-1]['id'])
+        wfs = get_workflows1(url, headers)
+    
+    wfs_list.extend(wfs)
+    return wfs_list
+
+def run(session_unixtime, dest_db, dest_table, api_endpoint='api.treasuredata.com', workflow_endpoint='api-workflow.treasuredata.com', count=100):
+    workflow_url = 'https://%s/api/workflows' % workflow_endpoint + '?count=%d&last_id=%s'
+    headers = {'Authorization': 'TD1 %s' % os.environ['TD_API_KEY']}
+    wfs_list = get_all_workflow(workflow_url, headers, count)
+    df = pd.DataFrame(wfs_list)
+    df['time'] = session_unixtime
+    client = pytd.Client(apikey=os.environ['TD_API_KEY'], endpoint='https://%s' % api_endpoint, database=dest_db)
+    client.load_table_from_dataframe(df, dest_table, if_exists='overwrite', fmt='msgpack')
