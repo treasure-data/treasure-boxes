6d52e9bb9c211d73275588c588b877857fe9fbb2
Makoto Yui <myui@apache.org>
2023-05-18 16:17:54 +0900

    Fixed a bug

--- machine-learning-box/automl/queries/auc.sql
+++ machine-learning-box/automl/queries/auc.sql
@@ -1,8 +1,7 @@
--- DIGDAG_INSERT_LINE
 select
   auc(prob, label) as auc
 from (
-  select predicted_proba as prob, ${target_column} as label
+  select predicted_proba as prob, if(cast(${target_column} as string)=="${positive_class}", 1, 0) as label
   from ${table}
   ORDER BY prob DESC
 ) t
